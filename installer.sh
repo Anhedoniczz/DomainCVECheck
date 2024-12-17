41bf95302c17f6365d6a8bb569b338eb64e5a4a1
Anhedonic <93049946+Anhedoniczz@users.noreply.github.com>
2024-12-17 23:05:11 +0400

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,10 @@
+echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list
+wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt upgrade
+sudo apt install golang-go
+sudo apt-get install httpx-toolkit
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/tomnomnom/assetfinder@latest
